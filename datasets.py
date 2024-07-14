717131af3a6750aa094914d7ea5da2a9040b648e
Sean <sean@Seans-MacBook-Air.local>
2024-07-14 18:23:32 +0800

    add "apply_center_mask" and "save samples"

--- datasets.py
+++ datasets.py
@@ -3,6 +3,7 @@ import random
 import os
 import numpy as np
 
+import torch
 from torch.utils.data import Dataset
 from PIL import Image
 import torchvision.transforms as transforms
@@ -28,12 +29,23 @@ class ImageDataset(Dataset):
         return masked_img, masked_part
 
     def apply_center_mask(self, img):
-        """Mask center part of image"""
+        """Mask center part of image(for face generation)"""
         # Get upper-left pixel coordinate
         i = (self.img_size - self.mask_size) // 2
         masked_img = img.clone()
         masked_img[:, i : i + self.mask_size, i : i + self.mask_size] = 1
 
+        return masked_img, i
+
+    def apply_frame_mask(self, img):
+        """Mask edge part of image(for frame generation)"""
+        mask = torch.ones_like(img)
+        i = (self.img_size - self.mask_size) // 2
+        mask[:, i : i + self.mask_size, i : i + self.mask_size] = 0
+        
+        masked_img = img * (1 - mask) + mask
+        # frame = img * mask
+        
         return masked_img, i
 
     def __getitem__(self, index):
@@ -45,9 +57,40 @@ class ImageDataset(Dataset):
             masked_img, aux = self.apply_random_mask(img)
         else:
             # For test data mask the center of the image
-            masked_img, aux = self.apply_center_mask(img)
+            masked_img, aux = self.apply_frame_mask(img)
+            # masked_img, aux = self.apply_center_mask(img)
 
         return img, masked_img, aux
 
     def __len__(self):
         return len(self.files)
+
+### Show the samples ###
+#     def visualize_samples(self, num_samples=5, save_dir='./'):
+#         os.makedirs(save_dir, exist_ok=True)
+#         for i in range(num_samples):
+#             img, masked_img, frame = self[i]
+            
+#             def tensor_to_pil(t):
+#                 return transforms.ToPILImage()(t.clamp(0, 1))
+            
+#             original_img = tensor_to_pil(img)
+#             masked_img = tensor_to_pil(masked_img)
+#             # frame = tensor_to_pil(frame)
+            
+#             combined_img = Image.new('RGB', (self.img_size * 3, self.img_size))
+#             combined_img.paste(original_img, (0, 0))
+#             combined_img.paste(masked_img, (self.img_size, 0))
+#             # combined_img.paste(frame, (self.img_size * 2, 0))
+            
+#             combined_img.save(os.path.join(save_dir, f'sample_{i}.png'))
+        
+#         print(f"Save to: {save_dir}")
+
+# if __name__ == "__main__":
+#     dataset = ImageDataset("data/img_align_celeba", transforms_=[
+#         transforms.Resize((128, 128)),
+#         transforms.ToTensor(),
+#     ])
+    
+#     dataset.visualize_samples(save_dir='./')
