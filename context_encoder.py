a3c015023d626609263429ba6eb62a482f097d3b
Sean-fn <seanfang@gmail.com>
2024-09-25 20:25:48 +0800

    Refactor config.py, and update context_encoder.py and datasets.py

--- context_encoder.py
+++ context_encoder.py
@@ -4,10 +4,12 @@ import os
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
+import torchvision
 from torchvision import transforms
 from torchvision.utils import save_image
 from PIL import Image
 import numpy as np
+from torch.utils.tensorboard import SummaryWriter
 
 from datasets import ImageDataset
 from models import Generator, Discriminator
@@ -36,6 +38,8 @@ class GANTrainer:
         self.d_losses = []
         self.g_adv_losses = []
         self.g_pixel_losses = []
+
+        self.writer = SummaryWriter(log_dir=f'logs/{config.opt.run_name}/')#{datetime.datetime.now().strftime("%Y%m%d-%H%M%S")}')
         
     def get_dataloader(self, mode="train"):
         transforms_ = [
@@ -67,18 +71,94 @@ class GANTrainer:
         sample = torch.cat((masked_samples, gen_fram, samples), -2)
         save_path = os.path.join(save_dir, f"{batches_done}.png")
         save_image(sample, save_path, nrow=6, normalize=True)
+    #     data = {
+    #         'epochs': np.array(self.epochs_list),
+    #         'd_losses': np.array(self.d_losses),
+    #         'g_adv_losses': np.array(self.g_adv_losses),
+    #         'g_pixel_losses': np.array(self.g_pixel_losses)
+    #     }
+    #     os.makedirs('./config_data', exist_ok=True)
+    #     np.save('./config_data/training_data.npy', data)
+
+    def log_training_progress(self, epoch, batch_idx, imgs, masked_imgs, masked_parts, 
+                              d_loss, g_adv, g_pixel, g_loss, global_step):
+        self.writer.add_scalar('Loss/Discriminator', d_loss.item(), global_step)
+        self.writer.add_scalar('Loss/Generator/Adversarial', g_adv.item(), global_step)
+        self.writer.add_scalar('Loss/Generator/Pixel', g_pixel.item(), global_step)
+        self.writer.add_scalar('Loss/Generator/Total', g_loss.item(), global_step)
+
+        if global_step % self.config.opt.sample_interval == 0:
+            self.log_images(imgs, masked_imgs, masked_parts, global_step)
+
+        # lr
+        self.writer.add_scalar('Learning Rate/Generator', self.optimizer_G.param_groups[0]['lr'], global_step)
+        self.writer.add_scalar('Learning Rate/Discriminator', self.optimizer_D.param_groups[0]['lr'], global_step)
+
+        self.log_gradients(global_step)
+
+    def log_images(self, imgs, masked_imgs, masked_parts, global_step):
+        with torch.no_grad():
+            fake_imgs = self.generator(masked_imgs)
+            img_grid = self.save_image_grid(fake_imgs, f"images/{global_step}.png")
+            self.writer.add_image('Generated Images', img_grid, global_step)
+
+            real_grid = self.save_image_grid(imgs, f"images/real_{global_step}.png")
+            masked_grid = self.save_image_grid(masked_imgs, f"images/masked_{global_step}.png")
+            self.writer.add_image('Real Images', real_grid, global_step)
+            self.writer.add_image('Masked Images', masked_grid, global_step)
+
+    def log_gradients(self, global_step):
+        for name, param in self.generator.named_parameters():
+            if param.grad is not None:
+                self.writer.add_histogram(f'Generator Gradients/{name}', param.grad, global_step)
+        
+        for name, param in self.discriminator.named_parameters():
+            if param.grad is not None:
+                self.writer.add_histogram(f'Discriminator Gradients/{name}', param.grad, global_step)
+
+    def log_epoch_end(self, epoch):
+        self.writer.add_histogram('Generator Weights', self.generator.state_dict()['weight'], epoch)
+        self.writer.add_histogram('Discriminator Weights', self.discriminator.state_dict()['weight'], epoch)
+
+    @staticmethod
+    def save_image_grid(img_tensor, filename, nrow=8):
+        img_grid = torchvision.utils.make_grid(img_tensor, nrow=nrow, normalize=True)
+        torchvision.utils.save_image(img_grid, filename)
+        return img_grid
+
+
+    def save_weights(self, epoch):
+        weights_dir = 'weights'
+        os.makedirs(weights_dir, exist_ok=True)
     
-    def save_training_data(self):
-        data = {
-            'epochs': np.array(self.epochs_list),
-            'd_losses': np.array(self.d_losses),
-            'g_adv_losses': np.array(self.g_adv_losses),
-            'g_pixel_losses': np.array(self.g_pixel_losses)
-        }
-        os.makedirs('./config_data', exist_ok=True)
-        np.save('./config_data/training_data.npy', data)
+        # make it pickable
+        # Save latest weights
+        torch.save(self.generator.state_dict(), os.path.join(weights_dir, 'generator_latest.pth'))
+        torch.save(self.discriminator.state_dict(), os.path.join(weights_dir, 'discriminator_latest.pth'))
+    
+        # Save epoch-specific weights
+        torch.save(self.generator.state_dict(), os.path.join(weights_dir, f'generator_epoch_{epoch}.pth'))
+        torch.save(self.discriminator.state_dict(), os.path.join(weights_dir, f'discriminator_epoch_{epoch}.pth'))
+
+        print(f"Saved model weights for epoch {epoch}")
 
     def train(self):
+        if self.config.opt.resume_training:
+            generator_path = os.path.join('weights', 'generator_latest.pth')
+            discriminator_path = os.path.join('weights', 'discriminator_latest.pth')
+        
+            if os.path.exists(generator_path):
+                self.generator.load_state_dict(torch.load(generator_path))
+                print("Loaded pre-trained generator weights")
+            else:
+                print("No pre-trained generator weights found. Starting from scratch.")
+        
+            if os.path.exists(discriminator_path):
+                self.discriminator.load_state_dict(torch.load(discriminator_path))
+                print("Loaded pre-trained discriminator weights")
+            else:
+                print("No pre-trained discriminator weights found. Starting from scratch.")
+
         for epoch in range(self.config.opt.n_epochs):
             for i, (imgs, masked_imgs, masked_parts) in enumerate(self.dataloader):
                 valid = torch.ones(imgs.shape[0], 1, int(self.config.opt.mask_size / 2 ** 3), int(self.config.opt.mask_size / 2 ** 3)).type(self.config.Tensor)
@@ -111,13 +191,21 @@ class GANTrainer:
                 )
 
                 # Save loss data into arrays
-                self.epochs_list.append(epoch + i / len(self.dataloader))
-                self.d_losses.append(d_loss.item())
-                self.g_adv_losses.append(g_adv.item())
-                self.g_pixel_losses.append(g_pixel.item())
+                # self.epochs_list.append(epoch + i / len(self.dataloader))
+                # self.d_losses.append(d_loss.item())
+                # self.g_adv_losses.append(g_adv.item())
+                # self.g_pixel_losses.append(g_pixel.item())
+
+                global_step = epoch * len(self.dataloader) + i
+                self.log_training_progress(epoch, i, imgs, masked_imgs, masked_parts, 
+                                           d_loss, g_adv, g_pixel, g_loss, global_step)
 
                 # Save after an amount of batches
                 batches_done = epoch * len(self.dataloader) + i
                 if batches_done % self.config.opt.sample_interval == 0:
                     self.save_sample(batches_done)
-                    self.save_training_data()
+                    self.save_weights(batches_done)
+                    # self.save_training_data()
+
+            self.log_epoch_end(epoch)
+        self.writer.close()
