e147aff12b01f33fdec6bfb226ccc2d46477f02a
Sean-fn <seanfang@gmail.com>
2024-09-28 18:25:09 +0800

    f/implemented carbs with CyclicLR, ReduceLROnPlateau

--- param_schedualer.py
+++ param_schedualer.py
@@ -7,17 +7,20 @@ from carbs import ObservationInParam
 from carbs import ParamDictType
 from carbs import Param
 
-def param_schedualer():
+def param_schedualer(config):
     param_spaces = [
-        Param(name="lr", space=LogSpace(scale=0.5), search_center=0.00009),
-        # Param(name="batch_size", space=LogSpace(is_integer=True, min=32, max=512), search_center=320),
-        # Param(name="n_epochs", space=LogSpace(is_integer=True, min=10, max=200), search_center=200),
+        Param(name="lr", space=LogSpace(scale=0.5), search_center=config.opt.lr),
+        Param(name="lr_min", space=LogSpace(scale=0.5, min=1e-5, max=1e-3), search_center=config.opt.lr_min),
+        Param(name="lr_max", space=LogSpace(scale=0.5, min=1e-3, max=1e-1), search_center=config.opt.lr_max),
+        Param(name="step_size_up", space=LogSpace(scale=0.5, min=100, max=1000), search_center=config.opt.step_size_up),
+        Param(name="plateau_factor", space=LogSpace(scale=0.5, min=0.1, max=0.9), search_center=config.opt.plateau_factor),
+        Param(name="plateau_patience", space=LogSpace(scale=1, min=2, max=10), search_center=config.opt.plateau_patience),
     ]
 
     carbs_params = CARBSParams(
-        better_direction_sign=-1,  # Assuming lower loss is better
+        better_direction_sign=-1,
         is_wandb_logging_enabled=False,
-        resample_frequency=0,  # Adjust as needed
+        resample_frequency=0,
     )
 
     return CARBS(carbs_params, param_spaces)
