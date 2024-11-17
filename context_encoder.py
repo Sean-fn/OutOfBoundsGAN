103a46ab7fd84650469f7cf1736b4047031695c5
Sean-fn <seanfang122@gmail.com>
2024-11-17 23:57:24 +0800

    refactor: streamline weight saving process; testing with CNN upsample

--- context_encoder.py
+++ context_encoder.py
@@ -40,7 +40,7 @@ class GANTrainer:
         self.g_adv_losses = []
         self.g_pixel_losses = []
 
-        self.writer = SummaryWriter(log_dir=f'logs/{config.opt.run_name}/ViT_BatchSize=4_{datetime.datetime.now().strftime("%Y%m%d-%H%M%S")}')
+        self.writer = SummaryWriter(log_dir=f'logs/{config.opt.run_name}/CNN_BatchSize=4_{datetime.datetime.now().strftime("%Y%m%d-%H%M%S")}')
         
     def get_dataloader(self, mode="train"):
         transforms_ = [
@@ -72,7 +72,7 @@ class GANTrainer:
         if global_step % self.config.opt.sample_interval == 0:
             self.log_images(global_step)
             self.log_gradients(global_step)
-            # self.save_weights(batches_done)
+            self.save_weights(global_step)
 
 
     def log_gradients(self, global_step):
@@ -84,22 +84,15 @@ class GANTrainer:
             if param.grad is not None:
                 self.writer.add_histogram(f'Discriminator Gradients/{name}', param.grad, global_step)
 
-    def log_epoch_end(self, epoch):
-        self.writer.add_histogram('Generator Weights', self.generator.state_dict()['weight'], epoch)
-        self.writer.add_histogram('Discriminator Weights', self.discriminator.state_dict()['weight'], epoch)
-
     def log_images(self, global_step):
         with torch.no_grad():
             samples, masked_samples, masked_parts = next(iter(self.test_dataloader))
 
-            valid = torch.ones(samples.shape[0], 1, int(self.config.opt.mask_size / 2 ** 3), int(self.config.opt.mask_size / 2 ** 3)).type(self.config.Tensor)
             samples = samples.type(self.config.Tensor)
             masked_samples = masked_samples.type(self.config.Tensor)
             i = 32  # TODO: make i a variable, passed to the model
             center_mask = self.config.opt.mask_size // 2
             gen_parts = self.generator(masked_samples)
-            g_adv = self.adversarial_loss(self.discriminator(gen_parts), valid)
-            g_pixel = self.pixelwise_loss(gen_parts, masked_parts)
             center_part = masked_samples.clone()
             gen_parts[:, :, i:i+center_mask, i:i+center_mask] = center_part[:, :, i:i+center_mask, i:i+center_mask]
             sample_imgs = torch.cat((masked_samples, gen_parts, samples), -2)
@@ -107,17 +100,15 @@ class GANTrainer:
             img_grid = torchvision.utils.make_grid(sample_imgs, nrow=4, normalize=True)
             torchvision.utils.save_image(img_grid, f"images/{global_step}.png")
             self.writer.add_image('Generated Images', img_grid, global_step)
-            self.writer.add_scalar('TestLoss/Generator/Adversarial', g_adv.item(), global_step)
-            self.writer.add_scalar('TestLoss/Generator/Pixel', g_pixel.item(), global_step)
 
 
     def save_weights(self, epoch):
         weights_dir = 'weights'
         os.makedirs(weights_dir, exist_ok=True)
 
-        if os.path.exists(os.path.join(weights_dir, 'generator_latest.pth')):
-            os.rename(os.path.join(weights_dir, 'generator_latest.pth'), os.path.join(weights_dir, f'generator_epoch_{epoch - self.config.opt.sample_interval}.pth'))
-            os.rename(os.path.join(weights_dir, 'discriminator_latest.pth'), os.path.join(weights_dir, f'discriminator_epoch_{epoch - self.config.opt.sample_interval}.pth'))
+        # if os.path.exists(os.path.join(weights_dir, 'generator_latest.pth')):
+        #     os.rename(os.path.join(weights_dir, 'generator_latest.pth'), os.path.join(weights_dir, f'generator_epoch_{epoch - self.config.opt.sample_interval}.pth'))
+        #     os.rename(os.path.join(weights_dir, 'discriminator_latest.pth'), os.path.join(weights_dir, f'discriminator_epoch_{epoch - self.config.opt.sample_interval}.pth'))
         torch.save(self.generator.state_dict(), os.path.join(weights_dir, 'generator_latest.pth'))
         torch.save(self.discriminator.state_dict(), os.path.join(weights_dir, 'discriminator_latest.pth'))
 
@@ -157,7 +148,6 @@ class GANTrainer:
             global_step = epoch * len(self.dataloader) + i
             self.log_training_progress(d_loss, g_adv, g_pixel, g_loss, global_step)
 
-        self.log_epoch_end(epoch)
         observed_value = g_loss.item()
         # carbs.observe(ObservationInParam(
         #     input=suggestion,
--- models.py
+++ models.py
@@ -33,42 +33,6 @@ class Generator(nn.Module):
             layers.append(nn.LeakyReLU(0.2))
             return layers
 
-        self.model = nn.Sequential(
-            *downsample(channels, 64, normalize=False),
-            *downsample(64, 64),
-            *downsample(64, 128),
-            *downsample(128, 256),
-            *downsample(256, 512),
-            nn.Conv2d(512, 4000, 1),
-        )
-        
-        self.upsample_blocks = nn.ModuleList([
-            VisionTransformerUpsample(4000, 512),
-            VisionTransformerUpsample(512, 256),
-            VisionTransformerUpsample(256, 128),
-            VisionTransformerUpsample(128, 128),    # For face common this
-            VisionTransformerUpsample(128, 64),
-        ])
-        
-        self.final_layers = nn.Sequential(
-            nn.Conv2d(64, channels, 3, 1, 1),
-            nn.Tanh()
-        )
-
-    def forward(self, x):
-        x = self.model(x)
-        for block in self.upsample_blocks:
-            x = checkpoint(block, x)
-        return self.final_layers(x)
-
-
-    #     def upsample(in_feat, out_feat, normalize=True):
-    #         layers = [nn.ConvTranspose2d(in_feat, out_feat, 4, stride=2, padding=1)]
-    #         if normalize:
-    #             layers.append(nn.BatchNorm2d(out_feat, 0.8))
-    #         layers.append(nn.ReLU())
-    #         return layers
-
     #     self.model = nn.Sequential(
     #         *downsample(channels, 64, normalize=False),
     #         *downsample(64, 64),
@@ -76,17 +40,53 @@ class Generator(nn.Module):
     #         *downsample(128, 256),
     #         *downsample(256, 512),
     #         nn.Conv2d(512, 4000, 1),
-    #         *upsample(4000, 512),
-    #         *upsample(512, 256),
-    #         *upsample(256, 128),
-    #         *upsample(128, 128),    # For face common this
-    #         *upsample(128, 64),
+    #     )
+        
+    #     self.upsample_blocks = nn.ModuleList([
+    #         VisionTransformerUpsample(4000, 512),
+    #         VisionTransformerUpsample(512, 256),
+    #         VisionTransformerUpsample(256, 128),
+    #         VisionTransformerUpsample(128, 128),    # For face common this
+    #         VisionTransformerUpsample(128, 64),
+    #     ])
+        
+    #     self.final_layers = nn.Sequential(
     #         nn.Conv2d(64, channels, 3, 1, 1),
     #         nn.Tanh()
     #     )
 
     # def forward(self, x):
-    #     return self.model(x)
+    #     x = self.model(x)
+    #     for block in self.upsample_blocks:
+    #         x = checkpoint(block, x)
+    #     return self.final_layers(x)
+
+
+        def upsample(in_feat, out_feat, normalize=True):
+            layers = [nn.ConvTranspose2d(in_feat, out_feat, 4, stride=2, padding=1)]
+            if normalize:
+                layers.append(nn.BatchNorm2d(out_feat, 0.8))
+            layers.append(nn.ReLU())
+            return layers
+
+        self.model = nn.Sequential(
+            *downsample(channels, 64, normalize=False),
+            *downsample(64, 64),
+            *downsample(64, 128),
+            *downsample(128, 256),
+            *downsample(256, 512),
+            nn.Conv2d(512, 4000, 1),
+            *upsample(4000, 512),
+            *upsample(512, 256),
+            *upsample(256, 128),
+            *upsample(128, 128),    # For face common this
+            *upsample(128, 64),
+            nn.Conv2d(64, channels, 3, 1, 1),
+            nn.Tanh()
+        )
+
+    def forward(self, x):
+        return self.model(x)
 
 
 
