7421b3feb967836ebcada6ec7ece813917ad297a
Sean-fn <seanfang122@gmail.com>
2024-11-17 22:01:25 +0800

    refactor: enhance logging process with threading and sample image logic; update Vision Transformer parameters

--- models.py
+++ models.py
@@ -3,9 +3,10 @@ import torch.nn.functional as F
 import torch
 from einops import rearrange
 from torch.nn import Transformer
+from torch.utils.checkpoint import checkpoint
 
 class VisionTransformerUpsample(nn.Module):
-    def __init__(self, in_feat, out_feat, nhead=8, num_layers=2, dim_feedforward=2048):
+    def __init__(self, in_feat, out_feat, nhead=4, num_layers=2, dim_feedforward=1024):
         super(VisionTransformerUpsample, self).__init__()
         self.conv = nn.ConvTranspose2d(in_feat, out_feat, kernel_size=4, stride=2, padding=1)
         self.transformer = Transformer(d_model=out_feat, nhead=nhead, num_encoder_layers=num_layers,
@@ -39,17 +40,54 @@ class Generator(nn.Module):
             *downsample(128, 256),
             *downsample(256, 512),
             nn.Conv2d(512, 4000, 1),
+        )
+        
+        self.upsample_blocks = nn.ModuleList([
             VisionTransformerUpsample(4000, 512),
             VisionTransformerUpsample(512, 256),
             VisionTransformerUpsample(256, 128),
             VisionTransformerUpsample(128, 128),    # For face common this
             VisionTransformerUpsample(128, 64),
+        ])
+        
+        self.final_layers = nn.Sequential(
             nn.Conv2d(64, channels, 3, 1, 1),
             nn.Tanh()
         )
 
     def forward(self, x):
-        return self.model(x)
+        x = self.model(x)
+        for block in self.upsample_blocks:
+            x = checkpoint(block, x)
+        return self.final_layers(x)
+
+
+    #     def upsample(in_feat, out_feat, normalize=True):
+    #         layers = [nn.ConvTranspose2d(in_feat, out_feat, 4, stride=2, padding=1)]
+    #         if normalize:
+    #             layers.append(nn.BatchNorm2d(out_feat, 0.8))
+    #         layers.append(nn.ReLU())
+    #         return layers
+
+    #     self.model = nn.Sequential(
+    #         *downsample(channels, 64, normalize=False),
+    #         *downsample(64, 64),
+    #         *downsample(64, 128),
+    #         *downsample(128, 256),
+    #         *downsample(256, 512),
+    #         nn.Conv2d(512, 4000, 1),
+    #         *upsample(4000, 512),
+    #         *upsample(512, 256),
+    #         *upsample(256, 128),
+    #         *upsample(128, 128),    # For face common this
+    #         *upsample(128, 64),
+    #         nn.Conv2d(64, channels, 3, 1, 1),
+    #         nn.Tanh()
+    #     )
+
+    # def forward(self, x):
+    #     return self.model(x)
+
 
 
 class Discriminator(nn.Module):
