6ffbd2e0a2bc57b6f1b198191e5819b11a980b8c
Sean-fn <seanfang122@gmail.com>
2024-11-18 17:11:10 +0800

    Update

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ class Config:
     def __init__(self):
         parser = argparse.ArgumentParser()
         # TODO: early stopping
-        parser.add_argument("--n_epochs", type=int, default=1)
-        parser.add_argument("--batch_size", type=int, default=4)
+        parser.add_argument("--n_epochs", type=int, default=5)
+        parser.add_argument("--batch_size", type=int, default=8)
         # parser.add_argument("--dataset_name", type=str, default="img_align_celeba")
         parser.add_argument("--dataset_name", type=str, default="street")
         parser.add_argument("--lr", type=float, default=0.00009)
@@ -19,12 +19,12 @@ class Config:
         parser.add_argument("--img_size", type=int, default=128)
         parser.add_argument("--mask_size", type=int, default=128)
         parser.add_argument("--channels", type=int, default=3)
-        parser.add_argument("--sample_interval", type=int, default=100)
-        parser.add_argument("--run_name", type=str, default="ArchiComp")
-        parser.add_argument("--last_log", type=str, default=max((os.path.join("logs", f) for f in os.listdir("logs")) if os.path.exists("logs") and os.listdir("logs") else ["logs/default"]), key=os.path.getmtime)
+        parser.add_argument("--sample_interval", type=int, default=1000)
+        parser.add_argument("--run_name", type=str, default="CNN_DynamicLR")
+        parser.add_argument("--last_log", type=str, default=max((os.path.join("logs", f) for f in os.listdir("logs")) if os.path.exists("logs") and os.listdir("logs") else ["logs/default"], key=os.path.getmtime))
         # TODO: resume training with one argument
         parser.add_argument("--resume_num", type=str, default="latest")
-        parser.add_argument("--resume_start_num", type=int, default=1)
+        parser.add_argument("--resume_start_num", type=int, default=1, help="Set to 0 if not resuming")
 
         parser.add_argument("--lr_min", type=float, default=1e-5)
         parser.add_argument("--lr_max", type=float, default=1e-3)
