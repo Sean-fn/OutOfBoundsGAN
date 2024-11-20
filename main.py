8de5b8207a4a04e6a0897fdf9af3659cfb0097af
Sean-fn <seanfang122@gmail.com>
2024-11-20 21:17:24 +0800

    Implement early stopping mechanism and enhance learning rate scheduling; Modify the ViT architacture

--- main.py
+++ main.py
@@ -10,18 +10,22 @@ from torch.utils.tensorboard import SummaryWriter
 
 def main():
     config = Config()
-    print(config.opt, ('-' for _ in range(len(15))))
+    print(config.opt)
 
     #BUG: has to be resume with the globo_step to continue the logging
-    writer = SummaryWriter(log_dir=config.opt.last_log if config.opt.resume_start_num != 0 
-                          else f'logs/{config.opt.run_name}_BatchSize={config.opt.batch_size}_{datetime.datetime.now().strftime("%m%d-%H%M")}')
+    # writer = SummaryWriter(log_dir=config.opt.last_log if config.opt.resume_start_num != 0 
+    #                       else f'logs/{config.opt.run_name}_BatchSize={config.opt.batch_size}_{datetime.datetime.now().strftime("%m%d-%H%M")}')
+    writer = SummaryWriter(log_dir=f'logs/cont_{config.opt.run_name}_BatchSize={config.opt.batch_size}_{datetime.datetime.now().strftime("%m%d-%H%M")}')
     trainer = GANTrainer(config, writer)
     # carbs = param_schedualer(config)
     if config.opt.resume_start_num != 0:
         load_weights(config, trainer)
-    for epoch in range(config.opt.n_epochs):
+
+    epoch = 0
+    while epoch < config.opt.n_epochs and not trainer.early_stopping:
         # trainer.train(epoch, carbs)
         trainer.train(epoch)
+        epoch += 1
     writer.close()
 
 if __name__ == '__main__':
