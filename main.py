fbecca3bce68ce28df4178f97395237e4d5439b8
Sean-fn <seanfang122@gmail.com>
2024-11-20 21:40:16 +0800

    Update

--- main.py
+++ main.py
@@ -17,13 +17,11 @@ def main():
     #                       else f'logs/{config.opt.run_name}_BatchSize={config.opt.batch_size}_{datetime.datetime.now().strftime("%m%d-%H%M")}')
     writer = SummaryWriter(log_dir=f'logs/cont_{config.opt.run_name}_BatchSize={config.opt.batch_size}_{datetime.datetime.now().strftime("%m%d-%H%M")}')
     trainer = GANTrainer(config, writer)
-    # carbs = param_schedualer(config)
     if config.opt.resume_start_num != 0:
         load_weights(config, trainer)
 
     epoch = 0
     while epoch < config.opt.n_epochs and not trainer.early_stopping:
-        # trainer.train(epoch, carbs)
         trainer.train(epoch)
         epoch += 1
     writer.close()
