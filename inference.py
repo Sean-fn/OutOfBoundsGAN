196e88cfb747b985de2c641f7fd9be8d350bf27e
Sean-fn <seanfang122@gmail.com>
2025-03-24 13:23:20 +0800

    Clean UP; Enhance inference script to improve error handling; update image generation process

--- inference.py
+++ inference.py
@@ -11,28 +11,40 @@ from models import Generator
 from datasets import ImageDataset
 from config import Config
 
+
 def main():
     config = Config()
     device = config.device
 
-    generator = Generator().to(device)
-    generator.eval()
+    try:
+        generator = Generator().to(device)
+        generator.eval()
 
-    generator.load_state_dict(torch.load('weights/generator_latest.pth', map_location=device))
+        state_dict = torch.load('./weights/ViT/generator_latest.pth', map_location=device, weights_only=True)
+        state_dict = torch.load('./weights/CNN_DynamicLR/generator_latest.pth', map_location=device, weights_only=True)
+        state_dict = torch.load('./weights/CNN/generator_latest.pth', map_location=device, weights_only=True)
+        generator.load_state_dict(state_dict)
+        print("Model loaded successfully!")
+
+    except Exception as e:
+        print(f"Error loading model: {e}")
 
     transforms_ = [
         transforms.Resize((256, 256), Image.BICUBIC),
         transforms.ToTensor(),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
     ]
+
+    # TODO: To better gives the path to the dataset
     dataset = ImageDataset("data/street", transforms_=transforms_, mode="val")
-    dataloader = DataLoader(dataset, batch_size=12, shuffle=False)
+    dataloader = DataLoader(dataset, batch_size=1, shuffle=False)
 
     imgs, masked_imgs, _ = next(iter(dataloader))
     imgs = imgs.to(device)
     masked_imgs = masked_imgs.to(device)
 
     # Inference
+    print('Generating images...')
     with torch.no_grad():
         gen_parts = generator(masked_imgs)
 
@@ -41,19 +53,23 @@ def main():
         center_mask = 128 // 2
         gen_parts[:, :, i:i+center_mask, i:i+center_mask] = center_part[:, :, i:i+center_mask, i:i+center_mask]
 
-    sample_imgs = torch.cat((masked_imgs.cpu(), center_part.cpu(), imgs.cpu()), -2)
+    print('Image generated successfully!')
+    sample_imgs = torch.cat((masked_imgs.cpu(), gen_parts.cpu(), imgs.cpu()), -2)
     img_grid = make_grid(sample_imgs, nrow=4, normalize=True)
 
-    plt.figure(figsize=(12, 8))
-    plt.imshow(img_grid.permute(1, 2, 0))
-    plt.axis('off')
-    plt.show()
+    # plt.figure(figsize=(12, 8))
+    # plt.imshow(img_grid.permute(1, 2, 0))
+    # plt.axis('off')
+    # plt.show()
 
-    save_option = input("Do you want to save the generated images? (y/n): ")
-    if save_option.lower() == 'y':
-        os.makedirs('inference_results', exist_ok=True)
-        save_image(img_grid, 'inference_results/generated.png')
-        print("Images saved to 'inference_results/generated.png'.")
+    # save_option = input("Do you want to save the generated images? (y/n): ")
+    # if save_option.lower() == 'y':
+    #     os.makedirs('inference_results', exist_ok=True)
+    #     save_image(img_grid, 'inference_results/generated.png')
+    #     print("Images saved to 'inference_results/generated.png'.")
+    os.makedirs('inference_results', exist_ok=True)
+    save_image(img_grid, 'inference_results/generated.png')
+    print("Images saved to 'inference_results/generated.png'.")
 
 if __name__ == "__main__":
     main()
